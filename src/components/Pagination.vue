a2b6bebb70976290cec2b4c6460d24431356c586
ChinniganCode <finbar.a.laffan@gmail.com>
2023-07-16 16:59:14 +1000

    FYP-01 - Fixed image layout

---
+++ src/components/Pagination.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex join w-full justify-center mt-4">
+    <button class="join-item btn" @click="decrementPage">«</button>
+    <button class="join-item btn">{{ currentPage }}</button>
+    <button class="join-item btn" @click="incrementPage">»</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const currentPage = ref(1);
+const emit = defineEmits(['page-change']);
+// Emit page change event
+watch(currentPage, () => {
+  emit('page-change', currentPage.value);
+});
+
+const decrementPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+};
+
+const incrementPage = () => {
+  currentPage.value++;
+};
+</script>

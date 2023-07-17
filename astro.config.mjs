5e40f85c300514e1519a9323cef7fca7afed4811
ChinniganCode <finbar.a.laffan@gmail.com>
2023-07-17 18:29:44 +1000

    Update astro config w/ node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import node from '@astrojs/node';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });

285d82bab4449fdd58a510a4f05d1b65bca54f8f
FinbarL <113319479+FinbarL@users.noreply.github.com>
2023-08-03 16:50:31 +1000

    Revert "implement auth"
    This reverts commit 97aee1cdf6f31b907dff18337c9b19e5c8875540.

--- src/utils/isLoggedIn.ts
+++
@@ -1,8 +0,0 @@
-
-export function isLoggedIn(cookie: string) {
-    console.log(cookie)
-    if(cookie.includes("isLoggedIn=true")) {
-        return cookie.split("username=")[1].split(";")[0];
-    }
-    return false;
-}

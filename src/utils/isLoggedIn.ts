5bbd3cff33718b9e0d10680aed2dc3c46d80dce5
FinbarL <113319479+FinbarL@users.noreply.github.com>
2023-08-03 17:02:34 +1000

    fix error handling, reintroduced commit

--- src/utils/isLoggedIn.ts
+++ src/utils/isLoggedIn.ts
@@ -1,6 +1,6 @@
 
 export function isLoggedIn(cookie: string) {
-    console.log(cookie)
+    console.log("COOKIE:",cookie)
     if(cookie && cookie.includes("isLoggedIn=true")) {
         return cookie.split("username=")[1].split(";")[0];
     }

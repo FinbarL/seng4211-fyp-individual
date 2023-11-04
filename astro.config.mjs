2f65d4e679a058d05f83e40bbb0e7d3d60e50cd1
FinbarL <finbar.laffan@mudbath.com.au>
2023-11-04 15:30:03 +1100

    Updated to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 
@@ -6,9 +6,23 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["stseng4211images.blob.core.windows.net"],
+    service: squooshImageService(),
+    remotePatterns: [{ protocol: "https" }],
+  },
   integrations: [tailwind(), vue()],
   output: 'server',
   adapter: vercel({
-    analytics: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+    imageService: true,
+    imagesConfig: {
+      sizes: [250, 500],
+    },
   }),
 });
